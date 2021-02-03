886515108e60622844ffc7eaf7f95d5c2a844e95
Tiberu05 <jclauc@gmail.com>
2021-02-03 13:10:27 +0200

    Minor bug

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const morgan = require('morgan');
+const path = require('path');
 const cors = require('cors');
 const fs = require('fs');
 require('dotenv').config();
